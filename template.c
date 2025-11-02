57e5ecb4d95595bf153e2e2cb67978ffce8aa54e
Vortron-rd <119626603+Vortron-rd@users.noreply.github.com>
2025-11-01 21:33:16 -0500

    Setup template program

--- template.c
+++ template.c
@@ -1,81 +1,82 @@
+/* debug-text.c ... */
+
+/*
+ * This example creates an SDL window and renderer, and then draws some text
+ * using SDL_RenderDebugText() every frame.
+ *
+ * This code is public domain. Feel free to use it for any purpose!
+ */
+
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-#define SDL_FLAGS SDL_INIT_VIDEO
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
 
-#define WINDOW_TITLE "Open Window"
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
 
-struct Game {
-        SDL_Window *window;
-        SDL_Renderer *renderer;
-};
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    SDL_SetAppMetadata("Example Renderer Debug Texture", "1.0", "com.example.renderer-debug-text");
 
-bool game_init_sdl(struct Game *g);
-void game_free(struct Game *g);
-void game_run(struct Game *g);
-
-bool game_init_sdl(struct Game *g) {
-    if (!SDL_Init(SDL_FLAGS)) {
-        fprintf(stderr, "Error initializing SDL3: %s\n", SDL_GetError());
-        return false;
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    g->window = SDL_CreateWindow(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
-    if (!g->window) {
-        fprintf(stderr, "Error creating Window: %s\n", SDL_GetError());
-        return false;
+    if (!SDL_CreateWindowAndRenderer("examples/renderer/debug-text", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
+    SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
-    g->renderer = SDL_CreateRenderer(g->window, NULL);
-    if (!g->renderer) {
-        fprintf(stderr, "Error creating Renderer: %s\n", SDL_GetError());
-        return false;
-    }
-
-    return true;
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
-void game_free(struct Game *g) {
-    if (g->renderer) {
-        SDL_DestroyRenderer(g->renderer);
-        g->renderer = NULL;
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
-
-    if (g->window) {
-        SDL_DestroyWindow(g->window);
-        g->window = NULL;
-    }
-
-    SDL_Quit();
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
-void game_run(struct Game *g) {
-    SDL_Delay(100);
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    const int charsize = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE;
 
-    SDL_RenderClear(g->renderer);
+    /* as you can see from this, rendering draws over whatever was drawn before it. */
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);  /* black, full alpha */
+    SDL_RenderClear(renderer);  /* start with a blank canvas. */
 
-    SDL_RenderPresent(g->renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);  /* white, full alpha */
+    SDL_RenderDebugText(renderer, 272, 100, "Hello world!");
+    SDL_RenderDebugText(renderer, 224, 150, "This is some debug text.");
 
-    SDL_Delay(5000);
+    SDL_SetRenderDrawColor(renderer, 51, 102, 255, SDL_ALPHA_OPAQUE);  /* light blue, full alpha */
+    SDL_RenderDebugText(renderer, 184, 200, "You can do it in different colors.");
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);  /* white, full alpha */
+
+    SDL_SetRenderScale(renderer, 4.0f, 4.0f);
+    SDL_RenderDebugText(renderer, 14, 65, "It can be scaled.");
+    SDL_SetRenderScale(renderer, 1.0f, 1.0f);
+    SDL_RenderDebugText(renderer, 64, 350, "This only does ASCII chars. So this laughing emoji won't draw: 🤣");
+
+    SDL_RenderDebugTextFormat(renderer, (float) ((WINDOW_WIDTH - (charsize * 46)) / 2), 400, "(This program has been running for %" SDL_PRIu64 " seconds.)", SDL_GetTicks() / 1000);
+
+    SDL_RenderPresent(renderer);  /* put it all on the screen! */
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
-int main(void) {
-    bool exit_status = EXIT_FAILURE;
-
-    struct Game game = {0};
-
-    if (game_init_sdl(&game)) {
-        game_run(&game);
-
-        exit_status = EXIT_SUCCESS;
-    }
-
-    game_free(&game);
-
-    return exit_status;
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    /* SDL will clean up the window/renderer for us. */
 }
